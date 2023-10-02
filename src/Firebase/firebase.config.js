7a53e8904ff5e521f3970caa3f4256b61134b016
henryjobel <jhobelsaha@gmail.com>
2023-10-03 02:57:18 +0600

    file created

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { getAuth,initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBmSIKaBpZslPYg-CGCIp-Bzelja-bGVxs",
+  authDomain: "dragon-news-auth-a4d22.firebaseapp.com",
+  projectId: "dragon-news-auth-a4d22",
+  storageBucket: "dragon-news-auth-a4d22.appspot.com",
+  messagingSenderId: "72333270711",
+  appId: "1:72333270711:web:4e464a1a0b4b37a635f732"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth;
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './Routes/Routes';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
