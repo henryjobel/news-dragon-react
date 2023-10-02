c5b474a1ab45ebe7337fc2a7303dd038c49ca251
henryjobel <jhobelsaha@gmail.com>
2023-10-03 05:32:36 +0600

    footer created

--- src/Layouts/Root.jsx
+++ src/Layouts/Root.jsx
@@ -1,11 +1,15 @@
 
 import { Outlet } from 'react-router-dom';
 import Header from '../Pages/Shared/Header/Header';
+import Footer from '../Pages/Footer/Footer';
 
 const Root = () => {
     return (
-        <div className='max-w-6xl mx-auto'>
+        <div>
+            <div className='max-w-6xl mx-auto'>
             <Outlet></Outlet>
+        </div>
+        <Footer></Footer>
         </div>
     );
 };
